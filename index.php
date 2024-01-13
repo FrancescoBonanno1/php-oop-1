1b11ba665b7954e9f99f7c6596e212a05be47bf1
FrancescoBonanno1 <francescosoapbonanno@gmail.com>
2024-01-13 21:08:41 +0100

    Costruttore Funzionante

--- index.php
+++ index.php
@@ -1,19 +1,14 @@
 <?php 
 require __DIR__ .'/models/Movies.php';
 
-$Movie1 = new Movie();
-$Movie1->title = "Apocalipse Now";
-$Movie1->genre = "Sci-fi";
-$Movie1->rating = "4.6/5";
-$Movie1->language ="English";
+$Movie1 = new Movie("Apocalypse-Now", "Sci-Fi", "4.6/5", "English");
+$Movie2 = new Movie("Madagascar", "Cartoon", "3.5/5", "Afrikaans");
+$Movie3 = new Movie("Cado Dalle Nubi", "Comedy", "5/5", "Italian");
 
-$Movie2 = new Movie();
-$Movie2->title = "Madagascar";
-$Movie2->genre = "Cartoon";
-$Movie2->rating = "3.5/5";
-$Movie2->language ="Afrikaans";
 
 
 var_dump($Movie1);
 echo $Movie1->getMoviedetails();
+var_dump($Movie2);
 echo $Movie2->getMoviedetails();
+echo $Movie3->getMoviedetails();
--- models/Movies.php
+++ models/Movies.php
@@ -9,4 +9,12 @@ public function getMoviedetails(){
     $result = $this-> title . ", " . $this-> genre .", " . $this-> rating . "- ". $this-> language . ".";
     return $result;
 }
+public function __construct($titolo, $genere, $punteggio, $lingua)
+{
+    $this->title = $titolo;
+    $this->genre = $genere;
+    $this->rating = $punteggio;
+    $this->language = $lingua;
+
+}
 }
