418491c9f0dd11459ebb4e517e21c597b6fb99a1
FrancescoBonanno1 <francescosoapbonanno@gmail.com>
2024-01-13 20:56:51 +0100

    metodo Creato

--- index.php
+++ index.php
@@ -7,4 +7,13 @@ $Movie1->genre = "Sci-fi";
 $Movie1->rating = "4.6/5";
 $Movie1->language ="English";
 
+$Movie2 = new Movie();
+$Movie2->title = "Madagascar";
+$Movie2->genre = "Cartoon";
+$Movie2->rating = "3.5/5";
+$Movie2->language ="Afrikaans";
+
+
 var_dump($Movie1);
+echo $Movie1->getMoviedetails();
+echo $Movie2->getMoviedetails();
--- models/Movies.php
+++ models/Movies.php
@@ -5,4 +5,8 @@ public $genre;
 public $rating;
 public $language;
 
+public function getMoviedetails(){
+    $result = $this-> title . ", " . $this-> genre .", " . $this-> rating . "- ". $this-> language . ".";
+    return $result;
+}
 }
